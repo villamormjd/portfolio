fd3698893d0e0ab57442f32e48015e26cd27abaa
villamormjd <villamormjd@gmail.com>
2019-11-23 13:32:55 +0800

    Added Download link and modify education models and template.

--- education/models.py
+++ education/models.py
@@ -5,13 +5,16 @@ from datetime import datetime
 class Education(models.Model):
     school = models.CharField(max_length=200)
     course = models.CharField(max_length=250, blank=True)
-    address = models.CharField(max_length=500)
+    address = models.CharField(max_length=500, blank=True)
+    awards = models.TextField(blank=True)
     start_date = models.DateTimeField(default=datetime.now, blank=True)
     end_date = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return self.school
 
-
     def year(self):
         return self.start_date.strftime("%Y") + "-" + self.end_date.strftime("%Y")
+
+    def award(self):
+        return self.awards.split("/")
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
+from django.urls import resolve
 from user_profile.models import UserProfile
 from jobs.models import Jobs
 from education.models import Education
@@ -10,10 +11,12 @@ def index(request):
     profile = UserProfile.objects.all()
     jobs = Jobs.objects.order_by('-hire_date')
     education = Education.objects.order_by('-start_date')
+    current_url = resolve(request.path_info).url_name
     context = {
         "users": profile[0],
         "jobs": jobs,
         "schools": education,
-        "skills": skills
+        "skills": skills,
+        "url": request.build_absolute_uri
     }
     return render(request, 'pages/index.html', context)
